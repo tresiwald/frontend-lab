88e649e4a6df7cec22c54bfe31592c24517f80f7
GauthamN <gauthamnekk@gmail.com>
2017-09-14 11:41:41 +0530

    Fixed error async problem

--- imports/ui/dashboard.js
+++ imports/ui/dashboard.js
@@ -1,7 +1,8 @@
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session'
-import Web3 from 'web3';
 import { setup, getConfig, transferTo} from '@melonproject/melon.js';
+import Web3 from 'web3';
+import callWithPromise from '../api/callWithPromise.js';
 import './dashboard.html';
 
 Template.dashboard.helpers({
@@ -17,13 +18,13 @@ Template.dashboard.events({
   'click .btn': async (event, instance) => {
     let captchaData = grecaptcha.getResponse();
     let defaultAccount = web3.eth.accounts[0];
-    Meteor.call('faucetRequest', captchaData, defaultAccount, function(error, result) {
-      grecaptcha.reset();
-      if (error) {
-        toastr.error('Please try again', error.reason)
-      } else {
-        toastr.success('You will receive 0.1 Kovan ETH and 10 Kovan MLN!', 'Success')
-      }
-    });
+    grecaptcha.reset();
+    try {
+      toastr.info('Please Wait');
+      let res = await callWithPromise('faucetRequest', captchaData, defaultAccount);
+      toastr.success('You have received 0.1 Kovan ETH and 10 Kovan MLN!', 'Success');
+    } catch(error) {
+      toastr.error('Please try again', error.reason);
+    }
   },
 });
--- server/main.js
+++ server/main.js
@@ -1,8 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+import { setup, getBalance, transferTo } from '@melonproject/melon.js';
 import BigNumber from "bignumber.js";
 import Web3 from 'web3';
 import verifyCaptcha from '../imports/api/verifyCaptcha.js';
-import { setup, getBalance, transferTo } from '@melonproject/melon.js';
 
 const fundingNode = '0x00c9D604ccF4Ed3f9cF735e9c3dea921F714B66F';
 
@@ -12,23 +12,22 @@ if (typeof web3 === 'undefined')
 setup.init({ web3, daemonAddress: fundingNode, defaultAccount: fundingNode});
 
 Meteor.methods({
-  faucetRequest: function(response, address) {
+  faucetRequest: async function(response, address) {
     if (verifyCaptcha(response, this.connection.clientAddress)) {
-      transferTo('MLN-T', address, 10)
+      return transferTo('MLN-T', address, 10)
       .then(transaction => {
         let amount = web3.toWei(0.1, 'ether');
         return web3.eth.sendTransaction({from:fundingNode, to:address, value:amount});
       })
       .then(send => {
-        console.log(send);
         return true;
       })
       .catch(error => {
-        throw new Meteor.Error(400, error);
+        throw new Meteor.Error(400, 'Transaction Error');
       });
     }
     else {
-      throw new Meteor.Error(400, "Captcha Error");
+      throw new Meteor.Error(400, 'Captcha Error');
     }
   }
 });
